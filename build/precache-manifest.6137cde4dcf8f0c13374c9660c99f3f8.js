9c63cae57ce2c032e2aedad377aa8f88e38c175b
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-05-14 08:07:02 -0300

    Remove /build from gitIgnore

---
+++ build/precache-manifest.6137cde4dcf8f0c13374c9660c99f3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fe6b8ee083fafbc3fe601faf02e521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4347259006b9535a9f5e",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6c032acf146cb49c7c35",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4347259006b9535a9f5e",
+    "url": "/static/js/2.85685f28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85685f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c032acf146cb49c7c35",
+    "url": "/static/js/main.741131f9.chunk.js"
+  },
+  {
+    "revision": "e68483044f8cf4030c2d",
+    "url": "/static/js/runtime-main.0027d731.js"
+  }
+]);
